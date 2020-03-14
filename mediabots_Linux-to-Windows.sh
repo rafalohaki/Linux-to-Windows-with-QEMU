c6b40417f2c686aaeac15f88e6fae03c7e6045b0
rafalohaki <rafalohaki@gmail.com>
2020-03-14 22:34:24 +0100

    Update mediabots_Linux-to-Windows.sh

--- mediabots_Linux-to-Windows.sh
+++ mediabots_Linux-to-Windows.sh
@@ -32,13 +32,13 @@ fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
-link1_status=$(curl -Is http://wpme.pl/iso/winxplite.iso | grep HTTP | cut -f2 -d" ")
+link1_status=$(curl -Is http://wpme.pl/iso/winxp.iso | grep HTTP | cut -f2 -d" ")
 link2_status=$(curl -Is https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://archive.org/download/WS2012R2/WS2012R2.ISO # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -P /mediabots http://wpme.pl/iso/winxplite.iso
+	sudo wget -P /mediabots http://wpme.pl/iso/winxp.iso
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots http://wpme.pl/iso/winxplite.iso
+	sudo wget -P /mediabots http://wpme.pl/iso/winxp.iso
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
